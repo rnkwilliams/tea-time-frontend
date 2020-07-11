bb75e4389ba707616a621b4ad83ea974321ce312
rnkwilliams <rnkwilliams07@gmail.com>
2020-07-10 19:09:27 -0500

    installed and imported packages to index.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-
+        <h1>Hello!</h1>
       </div >
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
