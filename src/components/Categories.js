5448c28357ad843aedadeb766cd6b58f01772155
rnkwilliams <rnkwilliams07@gmail.com>
2020-07-13 11:59:45 -0500

    updated categories component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -6,8 +6,10 @@ const Categories = (props) => {
     <div>
       {props.categories.map(category =>
         <div key={category.id}>{category.attributes.name}
-          <img src={category.attributes.img_url}
-            alt={category.attributes.name} />
+          <div>
+            <img src={category.attributes.img_url}
+              alt={category.attributes.name} />
+          </div>
         </div>)
       }
     </div >
