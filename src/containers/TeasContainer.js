13da1628bdafd660fa24f1a4cfc35d230ecd4bb2
rnkwilliams <rnkwilliams07@gmail.com>
2020-07-13 18:14:16 -0500

    created TeasContainer with child components, imported child components  and connection to state

--- src/containers/TeasContainer.js
+++ src/containers/TeasContainer.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchTeas } from '../actions/fetchTeas'
 import TeaForm from '../components/TeaForm'
+import Teas from '../components/Teas'
 
 
-export default class TeasContainer extends React.Component {
+class TeasContainer extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchTeas()
+  }
+
   render() {
     return (
       <div>
         <TeaForm />
+        <Teas teas={this.props.teas} />
       </div>
     )
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    teas: state.teas
+  }
+}
+
+export default connect(mapStateToProps, { fetchTeas })(TeasContainer);
