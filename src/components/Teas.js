c70ea634a49ea3dc799bed9f40d50298c80d6744
rnkwilliams <rnkwilliams07@gmail.com>
2020-07-16 09:50:59 -0500

    update Teas component, add import combineReducer

--- src/components/Teas.js
+++ src/components/Teas.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const Teas = (props) => {
-
+  //console.log(props)
   return (
     <div>
-      {/* {props.teas.map(tea =>
+      {props.teas.map(tea =>
         <div key={tea.id}>
           <h2>{tea.attributes.name}</h2>
           Tea Amount: {tea.attributes.amount} tsp<br></br>
@@ -14,7 +14,7 @@ const Teas = (props) => {
           Category: {tea.attributes.category.name}<br></br>
           Flavor Notes: {tea.attributes.notes}
         </div>
-      )} */}
+      )}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { combineReducers } from "redux";
+
 import thunk from 'redux-thunk'
+import App from './App';
 import categoryReducer from './reducers/categoryReducer'
 import teaReducer from './reducers/teaReducer'
 
-import App from './App';
+// const rootReducer = combineReducers({
+//   categoryReducer,
+//   teaReducer
+// })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(categoryReducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(teaReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
