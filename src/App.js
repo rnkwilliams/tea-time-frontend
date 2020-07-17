851e9f02a250ab465986a54ca18de4a0ad06d849
rnkwilliams <rnkwilliams07@gmail.com>
2020-07-17 15:59:03 -0500

    added and imported  NavBar and Home components to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { Route } from 'react-router-dom'
+import './App.css';
 import CategoriesContainer from './containers/CategoriesContainer'
 import TeasContainer from './containers/TeasContainer'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+
 
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <TeasContainer /><br></br>
+      <div className='App'>
+        <NavBar />
+        <Route exact path='/' component={Home} />
+        <TeasContainer />
         <CategoriesContainer />
       </div >
     );
