9759a09e481986ad0ff345fae6cad3121734ac2c
rnkwilliams <rnkwilliams07@gmail.com>
2020-07-20 17:30:43 -0500

    updated add new tea nav link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
     <div className='NavBar'>
       <NavLink to="/">TeaTime</NavLink>
       <NavLink to="/categories">Tea Library</NavLink>
-      <NavLink to="/teas/new">New Tea</NavLink>
+      <NavLink to="/teas/new">Add New Tea</NavLink>
       <NavLink to="/teas">My Teas</NavLink>
       <NavLink to="/timer">Timer</NavLink>
     </div>
