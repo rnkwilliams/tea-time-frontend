f40094fb7f767db7fea53125a51210ffd138ed6e
rnkwilliams <rnkwilliams07@gmail.com>
2020-07-21 14:25:05 -0500

    updated add/delete tea actions with history parameter and path

--- src/actions/addTea.js
+++ src/actions/addTea.js
@@ -1,5 +1,5 @@
 
-export const addTea = (data) => {
+export const addTea = (data, history) => {
 
   return (dispatch) => {
     fetch('http://localhost:3000/api/v1/teas', {
@@ -12,6 +12,7 @@ export const addTea = (data) => {
     })
       .then(resp => resp.json())
       .then(tea => dispatch({ type: 'ADD_TEA', payload: tea.data }))
+    history.push(`/teas`)
   }
 
 }
--- src/actions/deleteTea.js
+++ src/actions/deleteTea.js
@@ -1,9 +1,10 @@
-export const deleteTea = (teaId) => {
+export const deleteTea = (teaId, history) => {
   return (dispatch) => {
     fetch(`http://localhost:3000/api/v1/teas/${teaId}`, {
       method: 'DELETE'
     })
       .then(resp => resp.json())
       .then(tea => dispatch({ type: 'DELETE_TEA', payload: teaId }))
+    history.push(`/teas`)
   }
 }
