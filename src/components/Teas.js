0ab213fc91b0ff089a643fd0c37c5233734f5d6d
rnkwilliams <rnkwilliams07@gmail.com>
2020-07-22 17:52:47 -0500

    commented out tea attributes

--- src/components/Teas.js
+++ src/components/Teas.js
@@ -12,12 +12,12 @@ const Teas = (props) => {
           <Link to={`/teas/${tea.id}`}>
             <h2>{tea.attributes.name}</h2>
           </Link>
-          Tea Amount: {tea.attributes.amount} tsp<br></br>
+          {/* Tea Amount: {tea.attributes.amount} tsp<br></br>
           Water Amount: {tea.attributes.water} ml<br></br>
           Water Temperature: {tea.attributes.temp} F<br></br>
           Steep Time: {tea.attributes.time} mins<br></br>
           Category: {tea.attributes.category.name}<br></br>
-          Flavor Notes: {tea.attributes.notes}<br></br>
+          Flavor Notes: {tea.attributes.notes}<br></br> */}
         </div>
       )}
     </div>
