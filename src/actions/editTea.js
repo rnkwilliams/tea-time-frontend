54f927631313751cdd7b02454e3c0fc3cdb49e6a
rnkwilliams <rnkwilliams07@gmail.com>
2020-07-27 09:32:48 -0500

    updated editTea action and created setFormDataForEdit action to pre-fill values on edit form

--- src/actions/editTea.js
+++ src/actions/editTea.js
@@ -1,5 +1,5 @@
 export const editTea = (data, history) => {
-
+  //debugger;
   return (dispatch) => {
     fetch(`http://localhost:3000/api/v1/teas/${data.id}`, {
       method: 'PATCH',
@@ -10,7 +10,24 @@ export const editTea = (data, history) => {
       body: JSON.stringify(data)
     })
       .then(resp => resp.json())
-      .then(tea => dispatch({ type: 'EDIT_TEA', payload: tea }))
+      .then(tea => dispatch({ type: 'EDIT_TEA', payload: tea.data }))
+    history.push(`/teas/${data.id}`)
   }
+}
 
+export const setFormDataForEdit = tea => {
+  //debugger;
+  const teaFormData = {
+    name: tea.attributes.name,
+    time: tea.attributes.time,
+    amount: tea.attributes.amount,
+    temp: tea.attributes.temp,
+    water: tea.attributes.water,
+    notes: tea.attributes.notes,
+    category_id: tea.attributes.category_id
+  }
+  return {
+    type: "SET_FORM_DATA_FOR_EDIT",
+    teaFormData
+  }
 }
