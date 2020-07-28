228abacd3917583477abfa4ffaf79c30770155e8
rnkwilliams <rnkwilliams07@gmail.com>
2020-07-28 17:50:57 -0500

    commented out set form for data edit action

--- src/reducers/teaReducer.js
+++ src/reducers/teaReducer.js
@@ -13,10 +13,10 @@ export default function teaReducer(state = [], action) {
     case 'EDIT_TEA':
       return state.map(tea => tea.id === action.payload.id ? action.payload : tea)
 
-    case 'SET_FORM_DATA_FOR_EDIT':
-      //debugger;
-      //console.log(action.teaFormData)
-      return action.teaFormData
+    // case 'SET_FORM_DATA_FOR_EDIT':
+    //   //debugger;
+    //   //console.log(action.teaFormData)
+    //   return action.payload
 
     default:
       return state
