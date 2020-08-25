e83a1a744593dbaa17e378e467ab9d79e3e5a9b2
rnkwilliams <rnkwilliams07@gmail.com>
2020-08-25 13:39:32 -0500

    remove comments

--- src/components/teas/Tea.js
+++ src/components/teas/Tea.js
@@ -3,20 +3,6 @@ import { Link } from 'react-router-dom'
 
 const Tea = ({ tea }) => {
   return (
-    // tea ?
-    //   <div>
-    //     <h2>{tea.attributes.name}</h2>
-    //     Tea Amount: {tea.attributes.amount} tsp<br></br>
-    //     Water Amount: {tea.attributes.water} ml<br></br>
-    //     Water Temperature: {tea.attributes.temp} F<br></br>
-    //     Steep Time: {tea.attributes.time} mins<br></br>
-    //     Category: {tea.attributes.category.name}<br></br>
-    //     Flavor Notes: {tea.attributes.notes}<br></br>
-    //     <Link to={`/teas/${tea.id}/edit`}> Edit Tea</Link>
-    //   </div> :
-    //   <p>This tea does not exist!</p>
-
-
     tea ?
       <section className="page-section bg-light" id="portfolio">
         <div className="container">
