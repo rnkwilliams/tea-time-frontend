f686b64164f1c6a14790f294370438f2c20cee2b
rnkwilliams <rnkwilliams07@gmail.com>
2020-08-25 13:48:27 -0500

    remove comments

--- src/components/teas/Teas.js
+++ src/components/teas/Teas.js
@@ -1,27 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Rating from '../Rating'
 
 const Teas = (props) => {
-  //console.log(props)
-  //debugger;
 
   return (
-    // <div>
-    //   {props.teas.map(tea =>
-    //     <div key={tea.id}>
-    //       <Link to={`/teas/${tea.id}`}>
-    //         <h2>{tea.attributes.name}</h2>
-    //       </Link>
-    //       {/* Tea Amount: {tea.attributes.amount} tsp<br></br>
-    //       Water Amount: {tea.attributes.water} ml<br></br>
-    //       Water Temperature: {tea.attributes.temp} F<br></br>
-    //       Steep Time: {tea.attributes.time} mins<br></br>
-    //       Category: {tea.attributes.category.name}<br></br>
-    //       Flavor Notes: {tea.attributes.notes}<br></br> */}
-    //     </div>)}
-    // </div>
-
-    <section className="page-section bg-light" id="portfolio">
+    <section className="page-section bg-light" id="portfolio" >
       <div className="container">
         <div className="text-center">
           <h2 className="section-heading text-uppercase">Your Teas</h2>
@@ -38,11 +22,11 @@ const Teas = (props) => {
                       <img className="img-fluid" src="https://source.unsplash.com/FmgZ5xzDG-s/1600x900" alt="" />
                       <div className="portfolio-caption-heading">{tea.attributes.name}</div>
                     </Link>
+                    {/* <Rating tea={tea} key={tea.id} /> */}
                   </div>
                 </div>
               </div>
             </div>)}
-
         </div>
       </div>
     </section>
